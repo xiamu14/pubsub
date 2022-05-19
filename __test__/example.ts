1bb76159dceba81c00f1933896fa938a0741d681
ben <x_z_gfree@163.com>
2022-05-20 00:24:24 +0800

    修改 调整代码依赖包

--- __test__/example.ts
+++ __test__/example.ts
@@ -7,9 +7,6 @@ type events = {
 
 const pubSub = new PubSub<events>();
 
-// TS should error because "reason" doesn't exist on Key=CreatedPerson
-
-// TS should error because "reason" doesn't exist on Key=CreatedPerson
 const id = pubSub.subscribe(
   'CreatedPerson',
   async (message) => {
@@ -23,7 +20,6 @@ pubSub.publish('CreatedPerson', { id: '1', name: 'cory' });
 console.log(id);
 
 setTimeout(() => {
-  // pubSub.unsubscribe(id);
   // pubSub.unsubscribeAll();
   pubSub.publish('CreatedPerson', { id: '2', name: 'jane' });
 }, 200);
